e36ee9422930ae067f419a5d565051d33b16832c
YordanRD <tutorogamer@gmail.com>
2020-06-07 15:02:21 -0600

    Textos de las cartas, colocar textos en las cartas, desaparecer cartas con clic

--- DatosParty1/src/sample/Carta.java
+++ DatosParty1/src/sample/Carta.java
@@ -1,18 +1,26 @@
 package sample;
 
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class Carta {
 
-    private String descripcion;
+    private Label descripcion = new Label();
     private ImageView carta;
+    private bancoDeTextos banco = new bancoDeTextos();
 
-    public String getDescripcion() {
+    public Label getDescripcion() {
         return descripcion;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
+    public void setDescripcion(int tipoCarta){
+
+        descripcion.setLayoutX(280);
+        descripcion.setLayoutY(270);
+        descripcion.setText(banco.textoDeCartaTipo(tipoCarta));
+        descripcion.setFont(Font.font("Verdana", FontWeight.BOLD, 14));
     }
 
     public ImageView getCarta() {
@@ -22,7 +30,6 @@ public class Carta {
     public void setCarta(ImageView carta) {
         this.carta = carta;
     }
-
 
 
 
--- DatosParty1/src/sample/bancoDeTextos.java
+++ DatosParty1/src/sample/bancoDeTextos.java
@@ -2,14 +2,54 @@ package sample;
 
 public class bancoDeTextos {
 
+    public String textoDeCartaTipo(int tipo){
+        String random = new String();
+        if (tipo == 1){
+
+            random = cartasVERDES [(int)Math.random()*3];
+
+        }
+        if (tipo == 3){
+
+            random = cartasAZULES [(int)Math.random()*3];
+
+        }
+        if (tipo == 2){
+
+            random = cartasROJAS [(int)Math.random()*3];
+
+        }
+        if (tipo == 4){
+
+            random = cartasDORADAS [(int)Math.random()*3];
+
+        }
+        return random;
+    }
+
     private String[] cartasVERDES= new String[]
-            {"te encontraste una moneda en el suelo, eres 1% más rico",
-              "encontraste una moneda en tu bolsillo , eres 1% más rico",
-              "recibiste una herencia de una moneda"
-                                                        };
+            {"te encontraste una"+"\n"+ "moneda en el suelo,"+"\n"+"eres 1% más rico"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+
+                    "        +1 moneda",
+                    "encontraste una moneda"+"\n"+"en tu bolsillo,"+"\n"+"eres 1% más rico",
+                    "recibiste una herencia" +"\n"+"de una moneda"
+            };
 
-
-
+    private String[] cartasAZULES = new String[]
+            { "la vida no te sonrie,"+"\n"+ "pero tampoco es"+"\n"+"un mal dia",
+                    "Encuentras una bolsa vacia en el suelo, ahora tienes una bolsa",
+                    "Cero más cero, sigue siendo cero"
+            };
+    private String[] cartasROJAS = new String[]
+            { "Te asaltan"+"\n"+"amablemente,"+"\n"+"pierdes poco dinero"+"\n"+"\n"+"\n"+"\n"+"\n"+"\n"+
+                    "        -1 moneda",
+                    "Te da hambre, tienes que ir a comprar algo",
+                    "Te quito una moneda, solo por que quiero"
+            };
+    private String [] cartasDORADAS = new String []
+            { "Felicidades, encontraste un juego aleatorio, disfrutalo",
+                    "Tienes la oportunidad de ganar mas",
+                    ""
+            };
 
 
 }
